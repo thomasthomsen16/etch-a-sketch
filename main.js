6dfc335629d34fb9278544e40ddc6149803cd54d
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2024-09-03 12:39:35 +0200

    Add function to change background color for divs when mouse hover

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+// Creates div for grid
+
 function createDiv() {
     for (let i= 0; i< 256; i++) {
         const newDiv = document.createElement("div");
         const div = document.querySelector(".container");
         div.appendChild(newDiv);
     }
-    console.log("Div created");
+}
+function changeDivColor() {
+    // Iterates of all divs and add eventlistener for mouseover
+    const divs = document.querySelectorAll(".container div");
+    divs.forEach(div => {
+        div.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "blue";
+        })
+    })
 }
 
 createDiv();
+changeDivColor();
